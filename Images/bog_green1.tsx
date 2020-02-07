609bff098d5469a7f63dc0e57c284b15f4ca2347
JoshRak <48808816+JoshRak@users.noreply.github.com>
2020-02-07 13:50:56 -0500

    began map

---
+++ Images/bog_green1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="bog_green1" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../crawl-tiles Oct-5-2010/crawl-tiles Oct-5-2010/dc-dngn/floor/bog_green1.png" width="32" height="32"/>
+</tileset>
---
+++ Images/boots_iron2.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="boots_iron2" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../crawl-tiles Oct-5-2010/crawl-tiles Oct-5-2010/UNUSED/armour/boots_iron2.png"/>
+ </tile>
+</tileset>
