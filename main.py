6d7ea328e017c00f11497b17545d4c742e51566c
mswat123 <mananswat123@gmail.com>
2020-02-09 11:58:29 -0500

    Added player sprites and fixed wall collisions

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ gameMap = load_pygame("./Assets/Map/map.tmx")
 restingDownImage = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/restingDown.png").convert_alpha(), (32,32))
 walkingDown1Image = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/walkingDown1.png").convert_alpha(), (32,32))
 walkingDown2Image = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/walkingDown2.png").convert_alpha(), (32,32))
+restingUpImage = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/restingUp.png").convert_alpha(), (32,32))
+walkingUp1Image = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/walkingUp1.png").convert_alpha(), (32,32))
+walkingUp2Image = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/walkingUp2.png").convert_alpha(), (32,32))
+restingLeftImage = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/restingLeft.png").convert_alpha(), (32,32))
+walkingLeft1Image = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/walkingLeft1.png").convert_alpha(), (32,32))
+walkingLeft2Image = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/walkingLeft2.png").convert_alpha(), (32,32))
+restingRightImage = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/restingRight.png").convert_alpha(), (32,32))
+walkingRight1Image = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/walkingRight1.png").convert_alpha(), (32,32))
+walkingRight2Image = pygame.transform.scale(pygame.image.load("./Assets/Images/Sprites/Characters/walkingRight2.png").convert_alpha(), (32,32))
 class Sprite(object):
     def __init__(self):
         self.x = 250
@@ -37,7 +46,7 @@ class Sprite(object):
             if key[pygame.K_UP] and self.canMoveUp:
                 self.y -= dist
                 # self.velocityY = dist
-                self.image = restingDownImage
+                self.image = walkingUp1Image if self.movedLeft else walkingUp2Image
                 self.lastPressedButtons = "UP"
             elif key[pygame.K_DOWN] and self.canMoveDown:
                 self.y += dist
@@ -49,12 +58,12 @@ class Sprite(object):
             if key[pygame.K_LEFT] and self.canMoveLeft:
                 self.x -= dist
                 # self.velocityX = -dist
-                self.image = restingDownImage
+                self.image = walkingLeft1Image if self.movedLeft else walkingLeft2Image
                 self.lastPressedButtons = "LEFT"
             elif key[pygame.K_RIGHT] and self.canMoveRight:
                 self.x += dist
                 # self.velocityX = dist
-                self.image = restingDownImage
+                self.image = walkingRight1Image if self.movedLeft else walkingRight2Image
                 self.lastPressedButtons = "RIGHT"
             # else:
                 # self.velocityX = 0
@@ -62,11 +71,11 @@ class Sprite(object):
         else:
             # self.velocityX = self.velocityY = 0
             if self.lastPressedButtons == "LEFT":
-                pass
+                self.image = restingLeftImage
             elif self.lastPressedButtons == "UP":
-                pass
+                self.image = restingUpImage
             elif self.lastPressedButtons == "RIGHT":
-                pass
+                self.image = restingRightImage
             else:
                 self.image = restingDownImage
     def draw(self, surface):
@@ -87,24 +96,53 @@ def game_loop():
                     if tile:
                         gameScreen.blit(tile, (x * gameMap.tilewidth, y * gameMap.tileheight))
             elif isinstance(layer, pytmx.TiledObjectGroup):
-                if layer.name == 'Chests':
+                # if layer.name == 'Chests':
+                # sprite.canMoveRight = True
+                for obj in layer:
+                    if sprite.x + 42 > obj.x and sprite.y + 32 >= obj.y and sprite.y <= obj.y + obj.height and sprite.x < obj.x:
+                        sprite.canMoveRight = False
+                        print ("name: {} \ntype: {}".format(obj.name, obj.type))
+                        if obj.type == 'new room':
+                            print("You have entered the {}".format(obj.name))
+                        break
+                else:
                     for obj in layer:
-                        print ("obj: {},{}".format(obj.x, obj.y))
-                        print ("sprite: {},{}".format(sprite.x, sprite.y))
-                        if sprite.x + 42 > obj.x and sprite.y + 32 >= obj.y and sprite.y <= obj.y + 32 and sprite.x < obj.x:
-                            sprite.canMoveRight = False
-                        elif sprite.x < obj.x + 42 and sprite.y + 32 >= obj.y and sprite.y <= obj.y + 32 and sprite.x > obj.x:
+                        if sprite.x < obj.x + obj.width and sprite.y + 32 >= obj.y and sprite.y <= obj.y + obj.height and sprite.x > obj.x:
                             sprite.canMoveLeft = False
-                        else:
-                            sprite.canMoveLeft = True
-                            sprite.canMoveRight = True
-                        if sprite.y < obj.y + 42 and sprite.x + 32 >= obj.x and sprite.x <= obj.x + 32 and sprite.y > obj.y:
-                            sprite.canMoveUp = False
-                        elif sprite.y + 42 > obj.y and sprite.x + 32 >= obj.x and sprite.x <= obj.x + 32 and sprite.y < obj.y:
+                            print ("name: {} \ntype: {}".format(obj.name, obj.type))
+                            if obj.type == 'new room':
+                                print("You have entered the {}".format(obj.name))
+                            break
+                    else:
+                        sprite.canMoveLeft = True
+                        sprite.canMoveRight = True
+                # if any(sprite.x + 42 > obj.x and sprite.y + 32 >= obj.y and sprite.y <= obj.y + obj.height and sprite.x < obj.x for obj in layer):
+                #     if obj.type == 'new room':
+                #         print("You have entered the {}".format(obj.name))
+                #     sprite.canMoveRight = False
+                # elif any(sprite.x < obj.x + obj.width and sprite.y + 32 >= obj.y and sprite.y <= obj.y + height and sprite.x > obj.x for obj in layer):
+                #     sprite.canMoveLeft = False
+                # else:
+                #     sprite.canMoveLeft = True
+                #     sprite.canMoveRight = True
+                for obj in layer:
+                    if sprite.y < obj.y + obj.height and sprite.x + 32 >= obj.x and sprite.x <= obj.x + obj.width and sprite.y > obj.y:
+                        sprite.canMoveUp = False
+                        print ("name: {} \ntype: {}".format(obj.name, obj.type))
+                        if obj.type == 'new room':
+                            print("You have entered the {}".format(obj.name))
+                        break
+                else:
+                    for obj in layer:
+                        if sprite.y + 42 > obj.y and sprite.x + 32 >= obj.x and sprite.x <= obj.x + obj.width and sprite.y < obj.y:
                             sprite.canMoveDown = False
-                        else:
-                            sprite.canMoveDown = True
-                            sprite.canMoveUp = True
+                            print ("name: {} \ntype: {}".format(obj.name, obj.type))
+                            if obj.type == 'new room':
+                                print("You have entered the {}".format(obj.name))
+                            break
+                    else:
+                        sprite.canMoveUp = True
+                        sprite.canMoveDown = True
             elif isinstance(layer, pytmx.TiledImageLayer):
                 image = get_tile_image_by_gid(gid)
                 if image:
