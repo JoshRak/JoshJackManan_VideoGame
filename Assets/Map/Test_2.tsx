1cba2087a95e28354ce7358f4d299723d0eb1adf
Joshua Rakovitsky <josh.rakovitsky@outlook.com>
2020-02-18 11:49:28 -0500

    Fixes

---
+++ Assets/Map/Test_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Test_2" tilewidth="32" tileheight="32" tilecount="527" columns="31">
+ <image source="Tilesets/Test_2.png" width="992" height="544"/>
+</tileset>
